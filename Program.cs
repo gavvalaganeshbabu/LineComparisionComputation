2f4028878724fd83a0a2fc4d233d5ba51fb3b987
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 19:23:09 +0530

    Added Code to UC2

--- Program.cs
+++ Program.cs
@@ -7,24 +7,30 @@ namespace LineComparisionComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation :");
+            CheckTwoLinesEqual();
+        }
+        public static void CheckTwoLinesEqual() {
             Console.WriteLine("Calculating the Length of a Lines are Equal Or Not :");
             Console.WriteLine("Enter the Cooradinates of First Line  (x1,y1) and (x2,y2) ");
-            double x1=double.Parse(Console.ReadLine());
-            double y1=double.Parse(Console.ReadLine());
-            double x2=double.Parse(Console.ReadLine());
-            double y2=double.Parse(Console.ReadLine());           
-            double FirstLineLength=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double FirstLineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             Console.WriteLine("Enter the Cooradinates of Second Line  (x3,y3) and (x4,y4) ");
-            double x3=double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
             double y3 = double.Parse(Console.ReadLine());
             double x4 = double.Parse(Console.ReadLine());
             double y4 = double.Parse(Console.ReadLine());
             double SecondLineLength = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
-            
-            if (FirstLineLength.Equals(SecondLineLength)) {
-                Console.WriteLine("Two Line Length are Equal  "); }
-            else {
-                Console.WriteLine("Two Lines are Not Equal :"); 
+
+            if (FirstLineLength.Equals(SecondLineLength))
+            {
+                Console.WriteLine("Two Line Length are Equal  ");
+            }
+            else
+            {
+                Console.WriteLine("Two Lines are Not Equal :");
             }
         }
     }
